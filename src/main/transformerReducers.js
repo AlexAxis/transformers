0882b4564b49d2fa3eddb45d1faafad318dd2182
Alexis Sousa <alexis-sousa@hotmail.com>
2019-02-22 16:00:04 +0100

    Created input element / Created variable 'name' in the store (reducer and action) / Updated list in the table to be refreshed when input changes

--- src/main/transformerReducers.js
+++ src/main/transformerReducers.js
@@ -1,16 +1,18 @@
 const INITIAL_STATE = {
-    list: []
+    list: [],
+    name: ''
 }
 //as ACÇÕES
 //REDUCER (STATE,ACTION)
 export default function (state = INITIAL_STATE, action) {
 
     switch (action.type) {
+        case 'NAME_CHANGE':
+            return { ...state, name: action.payload }
         case 'TRANSFORMER_FETCH':
             return { ...state, list: action.payload }
+
         default:
             return state
     }
-
-
 }
--- src/transformersList/transformers.jsx
+++ src/transformersList/transformers.jsx
@@ -5,6 +5,7 @@ import { bindActionCreators } from 'redux'
 import { fetch } from './transformerActions'
 
 import List from './list'
+import SearchByName from './searchByName'
 
 class Transformers extends Component {
     constructor(props) {
@@ -20,6 +21,7 @@ class Transformers extends Component {
         return (
             <div>
                 <List />
+                <SearchByName />
             </div>
         )
     }
